29b3d9f3015c1b53a89d450238d40c7f725a0011
derek hecker <derekhecker8@gmail.com>
2020-06-03 21:12:27 -0400

    Coded Rectangle Class

--- Shapes/Rectangle.cs
+++ Shapes/Rectangle.cs
@@ -1,1 +1,38 @@
 using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Shapes
+{
+    class Rectangle
+    {
+        public double width;
+        public double height;
+        public Rectangle()
+        {
+            Console.WriteLine("Rectangle being created");
+            width = 1;
+            height = 1;
+        }
+        public Rectangle(double wide, double tall)
+        {
+            Console.WriteLine("Rectangle being created");
+            width = wide;
+            height = tall;
+        }
+        public Rectangle(Rectangle copy)
+        {
+            width = copy.width;
+            height = copy.height;
+        }
+        public double CalculateArea()
+        {
+            double a = width * height;
+            return a;
+        }
+        public double CalculatePermimeter()
+        {
+            double a = (width * 2)+(height*2);
+            return a;
+        }
+    }
+}
--- Shapes/Shapes.cs
+++ Shapes/Shapes.cs
@@ -3,13 +3,17 @@
 
 namespace Shapes
 {
-    class Program
+    class Shapes
     {
         static void Main(string[] args)
         {
             Square example = new Square(5);
             Console.WriteLine(example.CalculateArea());
             Console.WriteLine(example.CalculatePermimeter());
+
+            Rectangle Box = new Rectangle(3, 5);
+            Console.WriteLine(Box.CalculateArea());
+            Console.WriteLine(Box.CalculatePermimeter());
         }
     }
 }
